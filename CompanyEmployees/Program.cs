393a2b0fc8e6809e35ff8c2f0f00a90db99fbb3c
nagdeepanjan <mail@deepanjannag.com>
2025-10-20 22:51:59 -0400

    Add project files.

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,19 @@
+using CompanyEmployees.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
