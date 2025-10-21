ad2dd02fdae267d0b8c956865af96c4be87668fb
nagdeepanjan <mail@deepanjannag.com>
2025-10-21 13:40:20 -0400

    Add logging framework with new project structure
    Refactored solution to enhance architecture with new `Contracts` and `LoggerService` projects for better logging support. Updated `CompanyEmployees.csproj` to reference `LoggerService` and transitioned logging in `WeatherForecastController.cs` to use a custom `ILoggerManager`. Added `ServiceExtensions` for DI configuration and integrated NLog setup in `Program.cs` with a new `nlog.config`. Adjusted `launchSettings.json` to launch the browser automatically. Introduced `internallog.txt` for detailed internal logging operations. Established `LoggerManager` implementation for logging via NLog in `LoggerService`.

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,12 +1,17 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Logging
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 #region Services
 // Add services to the container.
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 builder.Services.AddControllers();
 #endregion
@@ -33,6 +38,19 @@ app.UseCors("CorsPolicy");
 
 
 app.UseAuthorization();
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    Console.WriteLine("Start of app.Use\n");
+    await next(context);
+    Console.WriteLine("End of app.Use\n");
+});
+
+//app.Run(async (HttpContext context) =>
+//{
+//    Console.WriteLine("Inside app.Run\n");
+//    //await context.Response.WriteAsync("Inside app.Run\n");
+//});
 
 app.MapControllers();
 #endregion
